a8666f029fff8e080884924685b8fd9765dcfe1f
pixeljogos <pixeljogoscontato@gmail.com>
2017-03-12 22:44:46 -0300

    Create posts.js

---
+++ css/posts.js
@@ -0,0 +1,8 @@
+$(&#39;.switch&#39;).on(&#39;click&#39;, function(e) {
+$(&#39;#fulljogo&#39;).toggleClass(&#39;jogo jogofull&#39;);
+$(&#39;#telapequena&#39;).toggleClass(&#39;switch switch2&#39;);
+    });  
+    $(&#39;.switch2&#39;).on(&#39;click&#39;, function(e) {
+$(&#39;#fulljogo&#39;).toggleClass(&#39;jogofull jogo&#39;);
+$(&#39;#telapequena&#39;).toggleClass(&#39;switch2 switch&#39;);
+    });
