8dc11babb41cc141af3893fc95f48d072d5f8475
pixeljogos <pixeljogoscontato@gmail.com>
2017-03-15 21:09:48 -0300

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,16 @@
 $('.switch').on('click', function(e) {
 $('#fulljogo').toggleClass('jogo jogofull');
 $('#telapequena').toggleClass('switch switch2');
+var elem = document.getElementById("myvideo");
+if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+} else if (elem.msRequestFullscreen) {
+  elem.msRequestFullscreen();
+} else if (elem.mozRequestFullScreen) {
+  elem.mozRequestFullScreen();
+} else if (elem.webkitRequestFullscreen) {
+  elem.webkitRequestFullscreen();
+}
 });
 $('.switch2').on('click', function(e) {
 $('#fulljogo').toggleClass('jogofull jogo');
